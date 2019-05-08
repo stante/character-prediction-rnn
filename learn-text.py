4e407e90ba9003bf79aeda0ddee7324cf5d0a3ce
Alexander Stante <stante@gmail.com>
2019-05-08 21:32:53 +0200

    Add function to read and clean text file

--- learn-text.py
+++ learn-text.py
@@ -1,4 +1,5 @@
 import click
+import re
 
 
 @click.command()
@@ -6,7 +7,27 @@ import click
 @click.argument('text-file')
 @click.argument('write-model')
 def main(epochs, text_file, write_model):
-    pass
+    corpus = read_text(text_file)
+    dictionary = set(corpus)
+    print(list(dictionary)[:1000])
+
+    print("Text file #words: {}".format(len(corpus)))
+    print("Dictionary size: {}".format(len(dictionary)))
+
+
+def read_text(file):
+    with open(file) as f:
+        s = f.read().lower()
+        s = s.replace('.', ' <fullstop> ')
+        s = s.replace(',', ' <comma> ')
+        s = s.replace('?', ' <question> ')
+        s = s.replace('!', ' <exclamation-mark>')
+        s = s.replace(';', ' <semicolon> ')
+        s = s.replace(':', ' <colon> ')
+        s = s.replace('„', ' <quote-start> ')
+        s = re.sub(r'\[.+\]', '', s)
+
+        return s.split()
 
 
 if __name__ == '__main__':
