28e04efdd0c02fc4954422888b14805f44a84ea7
Alexander Stante <stante@gmail.com>
2019-05-08 20:59:52 +0200

    Fix epoch option

--- learn-text.py
+++ learn-text.py
@@ -2,7 +2,7 @@ import click
 
 
 @click.command()
-@click.option('epochs')
+@click.option('--epochs')
 @click.argument('text-file')
 @click.argument('write-model')
 def main(epochs, text_file, write_model):
