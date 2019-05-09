7ebeb8b823a5b862b46b31b31fe836cdb7149bbe
Alexander Stante <stante@gmail.com>
2019-05-09 21:32:45 +0200

    Add returning hidden state

--- learn-text.py
+++ learn-text.py
@@ -32,7 +32,7 @@ def main(epochs, text_file, write_model):
             optimizer.zero_grad()
             x = one_hot_encoder(x, len(vocabulary))
             x, y = torch.from_numpy(x), torch.from_numpy(y)
-            output = model.forward(x, h)
+            output, h = model.forward(x, h)
             loss = criterion(output, y.view(8 * 20))
             loss.backward()
             optimizer.step()
--- model.py
+++ model.py
@@ -11,10 +11,10 @@ class RnnModel(nn.Module):
         self.output = nn.Linear(hidden_size, input_size)
 
     def forward(self, x, h):
-        x, _ = self.rnn(x, h)
+        x, hidden = self.rnn(x, h)
         x = self.output(x.contiguous().view(-1, self.hidden_size))
 
-        return x
+        return x, hidden
 
     def init_hidden(self):
         return torch.zeros(1, 8, self.hidden_size)
