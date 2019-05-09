25200879b73c215064dd277ae5dad84d6f39af7b
Alexander Stante <stante@gmail.com>
2019-05-09 21:15:01 +0200

    Add passing of hidden state to model

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 
 
 class RnnModel(nn.Module):
@@ -9,8 +10,11 @@ class RnnModel(nn.Module):
         self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
         self.output = nn.Linear(hidden_size, input_size)
 
-    def forward(self, x):
-        x = self.rnn(x)
-        x = self.output(x.view(-1, self.input_size))
+    def forward(self, x, h):
+        x, _ = self.rnn(x, h)
+        x = self.output(x.contiguous().view(-1, self.hidden_size))
 
         return x
+
+    def init_hidden(self):
+        return torch.zeros(1, 8, self.hidden_size)
