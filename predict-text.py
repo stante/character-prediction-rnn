21b111ad98afd3b3505b55b5847e55fefa1af073
Alexander Stante <stante@gmail.com>
2019-05-18 16:10:48 +0200

    Change output format of predicted text

--- predict-text.py
+++ predict-text.py
@@ -13,13 +13,13 @@ def main(n, read_model):
     model.load_state_dict(state['state_dict'])
     word2int = state['word2int']
     int2word = state['int2word']
-    initial = [word2int[c] for c in "lorem ipsum"]
+    initial = [word2int[c] for c in "jemand musste"]
     h = torch.zeros(1, 1, 30)
     for i in range(n):
         for c in initial:
             x = torch.randn(1, 1, 57)
             output, h = model.forward(x, h)
-            print(torch.argmax(F.softmax(output)).item())
+            print(int2word[torch.argmax(F.softmax(output, dim=1)).item()], end='')
 
 
 if __name__ == '__main__':
