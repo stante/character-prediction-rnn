96a400e04e832dcb9c44a21b4d9915485e39dac1
Alexander Stante <stante@gmail.com>
2019-05-18 20:36:34 +0200

    Change to larger hidden state of RNN and fix prediction code

--- predict-text.py
+++ predict-text.py
@@ -8,18 +8,28 @@ import torch.nn.functional as F
 @click.option('--n', default=100)
 @click.argument('read-model')
 def main(n, read_model):
-    model = RnnModel(57, 30)
+    model = RnnModel(57, 512)
     state = torch.load(read_model)
     model.load_state_dict(state['state_dict'])
     word2int = state['word2int']
     int2word = state['int2word']
     initial = [word2int[c] for c in "jemand musste"]
-    h = torch.zeros(1, 1, 30)
+    h = torch.zeros(1, 1, 512)
+
+    for c in initial:
+        print(int2word[c], end='')
+        x = torch.zeros(1, 1, 57)
+        x[0, 0, c] = 1
+        output, h = model.forward(x, h)
+
     for i in range(n):
-        for c in initial:
-            x = torch.randn(1, 1, 57)
-            output, h = model.forward(x, h)
-            print(int2word[torch.argmax(F.softmax(output, dim=1)).item()], end='')
+        k = torch.topk(F.softmax(output, dim=1), k=3)
+        i = torch.multinomial(k[0], 1).item()
+        c = k[1][0, i].item()
+        print(int2word[c], end='')
+        x = torch.zeros(1, 1, 57)
+        x[0, 0, c] = 1
+        output, h = model.forward(x, h)
 
 
 if __name__ == '__main__':
