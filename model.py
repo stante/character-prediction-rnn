2c22c293b15a0739cc8c4f18cd94ff5a80e54dfe
Alexander Stante <stante@gmail.com>
2019-05-19 20:15:43 +0200

    Add handling of variable layer size of recurrent layer

--- model.py
+++ model.py
@@ -3,11 +3,12 @@ import torch
 
 
 class RnnModel(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size, hidden_size, num_layers):
         super(RnnModel, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.rnn = nn.LSTM(input_size, hidden_size, num_layers=2, batch_first=True)
+        self.num_layers = num_layers
+        self.rnn = nn.LSTM(input_size, hidden_size, num_layers=num_layers, batch_first=True)
         self.output = nn.Linear(hidden_size, input_size)
 
     def forward(self, x, h):
@@ -17,4 +18,4 @@ class RnnModel(nn.Module):
         return x, hidden
 
     def init_hidden(self):
-        return (torch.zeros(2, 128, self.hidden_size), torch.zeros(2, 128, self.hidden_size))
+        return (torch.zeros(self.num_layers, 128, self.hidden_size), torch.zeros(self.num_layers, 128, self.hidden_size))
